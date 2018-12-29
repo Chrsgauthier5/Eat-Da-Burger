6383d709052d1b7a378d5bfb08cca098f765c266
Chris Gauthier <chrsgauthier5@gmail.com>
2018-12-29 08:07:29 -0500

    basic setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES 
+('Bacon Cheddar Cheeseburger', false),
+('Chicken and Waffle Burger', false), 
+('Black Bean Veggie Burger', false);
